8117900336ae396c46b600324149f3d550899f77
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2020-10-24 10:37:10 -0500

    updating page

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ $(document).ready(function(){
     $('#demo-carousel').carousel();
   });
 
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
