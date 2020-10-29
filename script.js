23426225a39e3fef774b154833b19b82480e66d1
Myles Burkemper <myles.burkemper@gmail.com>
2020-10-29 18:20:16 -0500

    last commit

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ $(document).ready(function(){
   $('.modal').modal();
 });
 $(document).ready(function() {
-  M.updateTextFields();
+  // M.updateTextFields();
+});
+var location = "kansas city"
+var queryURL = "https://api.unsplash.com/search/photos/?query=" + location + "&client_id=i7KYwCxnXSHUsg4lg9I2IhYYldXRCQIeeu-epexrwtA"
+
+$.ajax ({
+  url: queryURL,
+  method: 'GET',
+}).then(function (res) {
+  console.log('res :>> ', res)
 });
