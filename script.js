b40e7d40935c2590f6d7727ad192905c2648cb4e
Seth Glenn <Sbgees@yahoo.com>
2020-11-13 21:45:06 -0600

    added local storage logic to save most recent search to page on refresh

--- script.js
+++ script.js
@@ -3,54 +3,55 @@ $(document).ready(function () {
   $(".test").carousel();
   $(".modal").modal();
 
+
   //PIC API
 
-  function addModalImg(location){
+  function addModalImg(location) {
 
-  //var location = "london";
-  var queryPic =
-    "https://api.unsplash.com/search/photos/?query=" +
-    location +
-    "&client_id=i7KYwCxnXSHUsg4lg9I2IhYYldXRCQIeeu-epexrwtA"; 
-    console.log(queryPic)  
+    //var location = "london";
+    var queryPic =
+      "https://api.unsplash.com/search/photos/?query=" +
+      location +
+      "&client_id=i7KYwCxnXSHUsg4lg9I2IhYYldXRCQIeeu-epexrwtA";
+    console.log(queryPic)
 
-  $.ajax({
-    url: queryPic,
-    method: "GET",
-  }).then(function (res) {
-    console.log("res :>> ", res)
-    $(".carousel-item").empty();
-//  PIC 1 ****************
-    var d1 = $("<div>");
-    d1.attr("class","carousel test");
+    $.ajax({
+      url: queryPic,
+      method: "GET",
+    }).then(function (res) {
+      console.log("res :>> ", res)
+      $(".carousel-item").empty();
+      //  PIC 1 ****************
+      var d1 = $("<div>");
+      d1.attr("class", "carousel test");
 
-     for(var i = 0; i < res.results.length; i++){
-    var a = $("<a>");
-    if(i===0){
-      a.attr("class","carousel-item active");
+      for (var i = 0; i < res.results.length; i++) {
+        var a = $("<a>");
+        if (i === 0) {
+          a.attr("class", "carousel-item active");
 
-    }
-    a.attr("class","carousel-item");
-    a.attr("href","#"+(i+1)+"!");
-    a.attr("style","display:block")
+        }
+        a.attr("class", "carousel-item");
+        a.attr("href", "#" + (i + 1) + "!");
+        a.attr("style", "display:block")
 
-    var img = $("<img>");
-    img.attr("src", res.results[i].urls.small+".png");
+        var img = $("<img>");
+        img.attr("src", res.results[i].urls.small + ".png");
 
-    a.append(img);
- 
-     d1.append(a);
-     }
-     $(".ex1").append(d1);
+        a.append(img);
 
-  });
-}
+        d1.append(a);
+      }
+      $(".ex1").append(d1);
+
+    });
+  }
 
   // API information
-  
+
 
   function displayTripInfo() {
-  
+
     var city = $("#citySearch").val().trim();
     var queryURL =
       "https://public-api.blablacar.com/api/v2/trips?key=NGAGPgif8Qr9SQYIMftZVh64jQTSMcpG&fn=" +
@@ -63,44 +64,63 @@ $(document).ready(function () {
       console.log(response);
 
       var cityName = $("<h1>").html("City Name: " + city);
-      var date = $("<p>").html(
-        "Departure date: " + response.trips[0].departure_date
-      );
-      var currency = $("<p>").html(
-        "Currency Type: " + response.lowest_price_object.currency
-      );
-      var price = $("<p>").html(
-        "Price of Trip: " + response.trips[0].price.value
-      );
+
+      var date = $("<p>").html("Departure date: " + response.trips[0].departure_date);
+
+      var currency = $("<p>").html("Currency Type: " + response.lowest_price_object.currency);
+
+
+      var price = $("<p>").html("Price of Trip: " + response.trips[0].price.value);
 
       $(".flightInfo").append(cityName, date, currency, price);
 
 
-      
-      var stor = $(".flightInfo").html();
-      localStorage.setItem("trip", stor);
 
-      
-      // document.getElementsByClassName("flightInfo").innerHTML = localStorage.getItem("trip");
-      
-      
-      
-      
+      var stor = ["City Name: " + city,
+      "Departure date: " + response.trips[0].departure_date,
+      "Currency Type: " + response.lowest_price_object.currency,
+      "Price of Trip: " + response.trips[0].price.value,
+      ];
+
+      localStorage.setItem("citySearch2", JSON.stringify(stor));
+
+      // var object = localStorage.getItem(JSON.parse("trip"));
+
+      // document.getElementsByClassName("flightInfo").textContent = object;
+
+
+
+
+
+
+
     });
   }
 
-  document.getElementsByClassName("flightInfo").innerHTML = localStorage.getItem("trip");
 
   // localStorage.setItem("citySearch", location);
 
   // document.getElementsByClassName("flightInfo").innerHTML = localStorage.getItem("citySearch");
 
   // event handler to return API information
+  window.onload = function () {
+
+    var object = JSON.parse(localStorage.getItem("citySearch2"));
+
+    document.getElementById("cityClass").innerText = object[0];
+    document.getElementById("dateClass").innerText = object[1];
+    document.getElementById("currencyClass").innerText = object[2];
+    document.getElementById("priceClass").innerText = object[3];
+
+  }
+
+
   $(".search").on("click", function (event) {
     event.preventDefault();
 
     var city = $("#citySearch").val().trim();
     addModalImg(city);
+
 
     $(".flightInfo").empty();
     displayTripInfo(city);
